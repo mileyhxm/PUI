2c74d11694854e78ffbd333f70046b62a6d89485
Hu Xinmu <35498277+mileyhxm@users.noreply.github.com>
2021-12-03 17:19:28 -0500

    changes

--- homework8/assets/JS/main.js
+++ homework8/assets/JS/main.js
@@ -1,35 +1,10 @@
-// Splitting();
+// home page background color change 
 
-const [red, green, blue] = [46, 101, 243]
-// const section1 = document.querySelector('.section1')
 const card = document.querySelector('.cards-container');
-
-// card.addEventListener('scroll', function() {
-//   let x = 1 + (card.scrollLeft || window.pageXOffset) / 150;
-//   x = x < 1 ? 1 : x; // ensure x is always >= 1 (due to Safari's elastic scroll)
-//   const [r, g, b] = [red/x, green/x, blue/x].map(Math.round);
-//   const color = `rgb(${r}, ${g}, ${b})`
-//   document.body.style.backgroundColor = color;
-
-//   console.log(color);
-//   console.log(card.scrollLeft);
-  
-// })
-
-// card.addEventListener('scroll', function() {
-//     let x = card.scrollLeft;
-//     let w = window.innerWidth / 4;
-//     if (x < w) {
-//         document.body.style.backgroundColor = "#000000";
-//     } else {
-//         document.body.style.backgroundColor = "#ffffff";
-//     }
-//     console.log(x, w);
-// })
 
 card.addEventListener('scroll', function() {
 
-    scrollWidth = 1482 / 6;
+    scrollWidth = 1482 / 5;
     scrollLeft = card.scrollLeft;
     console.log(scrollWidth, scrollLeft);
 
@@ -41,34 +16,11 @@ card.addEventListener('scroll', function() {
         document.body.style.background = "#A852FF";
     } else if (scrollLeft >= scrollWidth * 3 && scrollLeft < scrollWidth * 4) {
         document.body.style.background = "#D64DAF";
-    } else {
+    } else if (scrollLeft >= scrollWidth * 4 && scrollLeft < scrollWidth * 5) {
         document.body.style.background = "#D64D4D";
+    } else if (scrollLeft >= scrollWidth * 5 && scrollLeft < scrollWidth * 6) {
+        document.body.style.background = "#E86329";
+    } else {
+        document.body.style.background = "#FFD656";
     }
-
-    // allCards = document.getElementsByClassName('card');
-
-    // for(i=0; i< allCards.length; i++ )
-    // {
-    //     curCard = allCards[i];
-        
-    //     // console.log(card.clientWidth, scrollLeft);
-        
-    //     // The code below makes the background color change when the 						scroll top passes the 2/3 of the previous div.
-    //     // console.log(curCard.offsetLeft, card.scrollLeft);
-    //     console.log(i);
-    //     widthBefore = 0;    
-    //     if (i > 0){
-    //         widthBefore = allCards[i-1].offsetWidth;
-    //     }
-
-    //     console.log(curCard.offsetLeft - widthBefore, scrollLeft);
-
-    //     if (scrollLeft > curCard.offsetLeft - widthBefore){
-    //         color = curCard.getAttribute("data-color");
-    //         document.body.style.background = color;
-    //         console.log(color);
-
-    //     }
-                
-    // }
 });
