308673f118e6a4b831ffcaf9d9c3855a8c7b1ad2
Hu Xinmu <35498277+mileyhxm@users.noreply.github.com>
2021-12-03 20:49:48 -0500

    accessibility fix 2

--- homework8/assets/JS/main.js
+++ homework8/assets/JS/main.js
@@ -1,6 +1,7 @@
 // home page background color change 
 
 const card = document.querySelector('.cards-container');
+const footer = document.getElementsByClassName('footer')[0];
 
 card.addEventListener('scroll', function() {
 
@@ -12,15 +13,21 @@ card.addEventListener('scroll', function() {
         document.body.style.background = "#2E65F3";
     } else if (scrollLeft >= scrollWidth * 1 && scrollLeft < scrollWidth * 2) {
         document.body.style.background = "#612EF3";
+        footer.style.color="white"
     } else if (scrollLeft >= scrollWidth * 2 && scrollLeft < scrollWidth * 3) {
         document.body.style.background = "#8e1dff";
+        footer.style.color="white"
     } else if (scrollLeft >= scrollWidth * 3 && scrollLeft < scrollWidth * 4) {
         document.body.style.background = "#E758BF";
+        footer.style.color="#111111"
     } else if (scrollLeft >= scrollWidth * 4 && scrollLeft < scrollWidth * 5) {
         document.body.style.background = "#F48178";
+        footer.style.color="#111111"
     } else if (scrollLeft >= scrollWidth * 5 && scrollLeft < scrollWidth * 6) {
         document.body.style.background = "#FF834E";
+        footer.style.color="#111111"
     } else {
-        document.body.style.background = "#FFD656";
+        document.body.style.background = "#ffe286";
+        footer.style.color="#111111"
     }
 });
