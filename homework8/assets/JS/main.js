4a2cf46ff59a23dfe23e509129828c4b54e58d87
Hu Xinmu <35498277+mileyhxm@users.noreply.github.com>
2021-12-03 20:37:25 -0500

    Accessibility fix

--- homework8/assets/JS/main.js
+++ homework8/assets/JS/main.js
@@ -15,11 +15,11 @@ card.addEventListener('scroll', function() {
     } else if (scrollLeft >= scrollWidth * 2 && scrollLeft < scrollWidth * 3) {
         document.body.style.background = "#8e1dff";
     } else if (scrollLeft >= scrollWidth * 3 && scrollLeft < scrollWidth * 4) {
-        document.body.style.background = "#D22CA3";
+        document.body.style.background = "#E758BF";
     } else if (scrollLeft >= scrollWidth * 4 && scrollLeft < scrollWidth * 5) {
-        document.body.style.background = "#d34242";
+        document.body.style.background = "#F48178";
     } else if (scrollLeft >= scrollWidth * 5 && scrollLeft < scrollWidth * 6) {
-        document.body.style.background = "#E86329";
+        document.body.style.background = "#FF834E";
     } else {
         document.body.style.background = "#FFD656";
     }
